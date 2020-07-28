a4fef1651256122aae569e4ba6b1d9880873de69
Atanu Paul <38816880+Atanu-Paul-au6@users.noreply.github.com>
2020-07-29 00:39:23 +0530

    Blog a simple React Redux app started and basic boiler plate done

---
+++ blog/src/components/PostList.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PostList extends Component {
+  render() {
+    return <div className="ui container">PostList</div>;
+  }
+}
+
+export default PostList;
---
+++ blog/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({
+  fakeReducer: () =>
+    "to remove the error message and get the basic app boiler plate working",
+});
